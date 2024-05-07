ae7e7689d98e644cecfbaefd9b4d964fc0341932
Rodgers Sande <rd_sande@yahoo.com>
2024-05-07 09:20:16 +0300

    Adding fixtures

--- resources/views/includes/fixtures/2024/may/fixtures.blade.php
+++ resources/views/includes/fixtures/2024/may/fixtures.blade.php
@@ -2,8 +2,46 @@
     <div class="uk-panel">
         <div class="inner-fixtures">
             <h3><img src="{{asset('img/league/nbc.png')}}" class="compt-logo"> NBC PREMIER LEAGUE </h3>
-            <p> WED 01/05/2024</p>
-            <p> AZAM COMPLEX, CHAMAZI</p>
+            <p> 06/05/2014 </p>
+            <p> MOROGORO</p>
+            <div class="teams-inner-container">
+                <div class="team-disp-1">
+                    <img src="{{ asset('img/teamlogos/mtibwa.png')}}" class="logo-card-item" alt="Team Logo" />
+                </div>
+                <div class="team-disp-1 results-section">
+                    <div class="scr-card">
+                        <span class="hm-team-score">
+                            0
+                        </span>
+                        :
+                        <span class="hm-team-score">
+                            2
+                        </span>
+
+                    </div>
+                </div>
+                <div class="team-disp-1">
+                    <img src="{{ asset('img/logo.png')}}" class="logo-card-item" alt="Team Logo" />
+                </div>
+            </div>
+            <div class="fxture">
+                <span class="homteam">
+                    MTIBWA SUGAR
+                </span>
+                <span class="faintcol"> VS </span>
+                <span class="homteam">
+                    AZAM FC
+                </span>
+            </div>
+        </div>
+    </div>
+</li>
+<li>
+    <div class="uk-panel">
+        <div class="inner-fixtures">
+            <h3><img src="{{asset('img/league/nbc.png')}}" class="compt-logo"> NBC PREMIER LEAGUE </h3>
+            <p> 09/05/2024</p>
+            <p>MKAPA STADIUM</p>
             <div class="teams-inner-container">
                 <div class="team-disp-1">
                     <img src="{{ asset('img/logo.png')}}" class="logo-card-item" alt="Team Logo" />
@@ -33,8 +71,9 @@
                     SIMBA SC
                 </span>
             </div>
-<!--             <a href="{{ url('match')}}" class="match-btn"><button class="uk-button uk-button-primary">Match Details</button></a>
--->        </div>
+            <!--             <a href="{{ url('match')}}" class="match-btn"><button class="uk-button uk-button-primary">Match Details</button></a>
+-->
+        </div>
     </div>
 </li>
 <li>
@@ -72,8 +111,9 @@
                     AZAM FC
                 </span>
             </div>
-<!--             <a href="{{ url('match')}}" class="match-btn"><button class="uk-button uk-button-primary">Match Details</button></a>
--->        </div>
+            <!--             <a href="{{ url('match')}}" class="match-btn"><button class="uk-button uk-button-primary">Match Details</button></a>
+-->
+        </div>
     </div>
 </li>
 <li>
@@ -111,8 +151,9 @@
                     AZAM FC
                 </span>
             </div>
-<!--             <a href="{{ url('match')}}" class="match-btn"><button class="uk-button uk-button-primary">Match Details</button></a>
--->        </div>
+            <!--             <a href="{{ url('match')}}" class="match-btn"><button class="uk-button uk-button-primary">Match Details</button></a>
+-->
+        </div>
     </div>
 </li>
 <li>
@@ -150,8 +191,9 @@
                     KAGERA SUGAR
                 </span>
             </div>
-<!--             <a href="{{ url('match')}}" class="match-btn"><button class="uk-button uk-button-primary">Match Details</button></a>
--->        </div>
+            <!--             <a href="{{ url('match')}}" class="match-btn"><button class="uk-button uk-button-primary">Match Details</button></a>
+-->
+        </div>
     </div>
 </li>
 <li>
@@ -189,7 +231,8 @@
                     AZAM FC
                 </span>
             </div>
-<!--             <a href="{{ url('match')}}" class="match-btn"><button class="uk-button uk-button-primary">Match Details</button></a>
--->        </div>
+            <!--             <a href="{{ url('match')}}" class="match-btn"><button class="uk-button uk-button-primary">Match Details</button></a>
+-->
+        </div>
     </div>
 </li>
